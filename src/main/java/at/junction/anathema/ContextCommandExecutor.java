45ec870ca3dcdce4a6a11aa77d561635fb7c80cd
HansiHE <hansihe@hansihe.com>
2012-12-09 21:20:27 +0100

    forgot to add refactored files

---
+++ src/main/java/at/junction/anathema/ContextCommandExecutor.java
@@ -0,0 +1,13 @@
+package at.junction.anathema;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ContextCommandExecutor  implements CommandExecutor  {
+
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+		return false;
+	}
+
+}
